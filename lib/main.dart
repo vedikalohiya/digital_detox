ef595f7a082cb1199c288d92e854cf6d3bb46eb6
Vedika Lohiya <vedika.lohiya22@gmail.com>
2025-09-10 15:18:53 +0530

    Title added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Login App',
+      title: 'Login Page',
       home: LoginPage(),
     );
   }
