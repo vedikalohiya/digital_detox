ea9a874c98efe9d4a5e9ead8a00bf8d53c6402e8
Vedika Lohiya <vedika.lohiya22@gmail.com>
2025-11-21 16:50:56 +0530

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
